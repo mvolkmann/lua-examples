23883b5b5b311dcaabac428d1012780838316492
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-25 15:02:46 -0500

    progress

--- love/monkey-nim/src/conf.lua
+++ love/monkey-nim/src/conf.lua
@@ -1,8 +1,9 @@
 local love = require "love"
 
 function love.conf(t)
+  t.modules.joystick = false
   t.title = "Monkey Nim"
-  t.version = "11.4"    -- version of Love2d
+  t.version = "11.4"    -- version of Love2D
   t.window.width = 393  -- third of 1179 (iPhone 14 Pro width)
   t.window.height = 852 -- third of 2556 (iPhone 14 Pro height)
 end
